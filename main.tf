2e82e8f7ce547f09a09407e3befcabc41f7a31c9
Rob de Rijk <rob.derijk@harborn.com>
2022-07-02 22:36:22 +0200

    Try log bucket.

--- main.tf
+++ main.tf
@@ -85,26 +85,15 @@ data "aws_iam_policy_document" "s3_bucket_policy" {
   }
 }
 
-data "aws_canonical_user_id" "current" {}
-
 module "log_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 2.0"
+  source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "logs-static-hosting-cdn"
-  acl    = null
-  grant = [{
-    type        = "CanonicalUser"
-    permissions = ["FULL_CONTROL"]
-    id          = data.aws_canonical_user_id.current.id
-  }, {
-    type        = "CanonicalUser"
-    permissions = ["FULL_CONTROL"]
-    id          = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
-    # Ref. https://github.com/terraform-providers/terraform-provider-aws/issues/12512
-    # Ref. https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html
-  }]
-  force_destroy = true
+  bucket ="static-hosting-cdn-logs"
+  acl    = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
 
 module "cdn" {
